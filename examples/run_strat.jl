913887ff1c8de0690336bbdd6f34f9afcb45766c
dysonance <jamos125@gmail.com>
2017-07-18 05:23:07 -0400

    trimming up the example a bit

--- examples/run_strat.jl
+++ examples/run_strat.jl
@@ -1,34 +1,16 @@
 using Temporal, Indicators
 
-function indfun(universe::Dict{Symbol,Asset}, asset_id::Symbol)::TS{Float64}
-    x = universe[asset_id].data[:AdjClose]
-    y = sma(x, n=200)
-    y.fields[1] = :SMA200
-    return y
-end
+# 0. initialize the accounting work (blotter, account, portfolio, orders)
 
-for (ticker, asset) in universe
-    universe[ticker].data = [universe[ticker].data indfun(universe, ticker)]
-end
+# 1. define the universe of assets
 
-function sigfun(universe::Dict{Symbol,Asset}, asset_id::Symbol)::TS{Bool}
-    a = universe[id].data[[:AdjClose,:SMA200]]
-    b = crossover(a[:AdjClose].values, a[:SMA200].values)
-    c = crossunder(a[:AdjClose].values, a[:SMA200].values)
-    return ts([b c], a.index, [:Long,:Sell])
-end
+# 2. define the indicators
 
-signals = Dict{Symbol,TS}()
-for (ticker, asset) in universe
-    signals[ticker] = sigfun(universe, ticker)
-end
+# 3. define the signals
+# go long when close crosses over 200-day sma, sell when it crosses under
+sig_long = Signal(:(@cxo :AdjClose :SMA200), :Long)
+sig_sell = Signal(:(@cxu :AdjClose :SMA200), :Sell)
+sig_vec = [sig_long, sig_sell]
 
-# this is the function that generates the orders to be parsed later
-function rulefun(signals::Dict{Symbol,Asset}, asset_id::Symbol, t::T)::Void where {T<:Dates.TimeType}
-    s = signals[id]
-    if s[t,:Long].values[1]
-        order!(B, asset_id, 100, t)
-    elseif s[t,:Sell].values[1]
-        order!(B, asset_id, -100, t)
-    end
-end
+# 4. define the trading rules
+
