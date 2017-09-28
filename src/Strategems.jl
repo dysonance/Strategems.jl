c5e5fbb09416b29335f4a9961c50dc8906a16ddc
dysonance <jamos125@gmail.com>
2017-09-27 21:52:50 -0400

    laying groundwork for package dev

--- src/Strategems.jl
+++ src/Strategems.jl
@@ -1,3 +1,19 @@
+VERSION >= v"0.6-" && __precompile__(true)
+
 module Strategems
+using Base.Dates
+using Temporal
+using Indicators 
+using RecipesBase
+
+export
+    # parameter sets
+    ps, get_n_runs, get_param_combos, get_run_params
+    # indicators
+    # strategies
+    # portfolios
+    # accounting
+
+include("paramset.jl")
 
 end
