8141b0af722f2d5cedd7b5876e22c8b0758634ea
dysonance <jamos125@gmail.com>
2017-09-28 03:33:57 -0400

    bugfix after running tests when generate signals

--- src/strategy.jl
+++ src/strategy.jl
@@ -16,7 +16,7 @@ function generate_trades(strat::Strategy)::Dict{String,TS}
     for asset in strat.universe.assets
         trades[asset] = TS()
         for rule_name in keys(strat.rules)
-            trigger = prep_trigger(rules[rule_name], indicators[asset])
+            trigger = prep_trigger(strat.rules[rule_name], strat.indicators[asset])
             trades[asset] = [trades[asset] eval(trigger)]
             trades[asset].fields[end] = rule_name
         end
