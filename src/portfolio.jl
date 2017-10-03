3ca6f0168b48f893a92973b26fb342b993256d43
dysonance <jamos125@gmail.com>
2017-10-03 06:58:41 -0400

    bugfixes in portfolio constructor

--- src/portfolio.jl
+++ src/portfolio.jl
@@ -5,15 +5,15 @@ type and methods to track the evolution of a strategy's securities portfolio com
 mutable struct Portfolio
     quantity::Matrix{Float64}
     weight::Matrix{Float64}
-    entered::Matrix{Float64}
-    settled::Matrix{Float64}
+    entry_price::Matrix{Float64}
+    close_price::Matrix{Float64}
     pnl::Matrix{Float64}
     idx::Vector{<:TimeType}
     function Portfolio(universe::Universe)
         idx = get_overall_index(universe)
         quantity = weight = entry_price = close_price = pnl =
             zeros(Float64, length(idx), length(universe.assets))
-        return new(quantity, weight, entered, settled, pnl, idx)
+        return new(quantity, weight, entry_price, close_price, pnl, idx)
     end
 end
 
