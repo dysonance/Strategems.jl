d69748a8ee03c469eaafeacf7159b01cdf682f2b
dysonance <jamos125@gmail.com>
2017-10-26 03:22:21 -0400

    updated mama example -- more flexible data sourcing/gathering function

--- examples/mama.jl
+++ examples/mama.jl
@@ -4,12 +4,25 @@ using Base.Test
 # define universe and gather data
 assets = ["CHRIS/CME_CL1", "CHRIS/CME_RB1"]
 universe = Universe(["CHRIS/CME_CL1", "CHRIS/CME_RB1"])
-gather!(universe)
+function datasource(asset::String; save_downloads::Bool=true)::TS
+    savedata_path = Pkg.dir("Strategems", "data/$asset.csv")
+    if isfile(savedata_path)
+        return Temporal.tsread(savedata_path)
+    else
+        X = quandl(asset)
+        if save_downloads
+            Temporal.tswrite(X, savedata_path)
+        end
+        return X
+    end
+end
+gather!(universe, source=datasource)
 
 # define indicators and parameter space
 arg_names = [:fastlimit, :slowlimit]
 arg_defaults = [0.5, 0.05]
-paramset = ParameterSet(arg_names, arg_defaults)
+arg_ranges = [0.01:0.01:0.99, 0.01:0.01:0.99]
+paramset = ParameterSet(arg_names, arg_defaults, arg_ranges)
 f(x; args...) = Indicators.mama(Temporal.hl2(x); args...)
 indicator = Indicator(f, paramset)
 
