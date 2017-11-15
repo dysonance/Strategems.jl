2ef2e68f277be2f332e63e9c10a023480059bd70
dysonance <jamos125@gmail.com>
2017-11-15 07:43:06 -0500

    updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,35 +3,68 @@ using Base.Test
 
 # define universe and gather data
 assets = ["Corn"]
-universe = Universe(assets)
-@test universe.assets == assets
-gather!(universe, source=(asset)->Temporal.tsread("$(Pkg.dir("Temporal"))/data/$asset.csv"))
-@test length(setdiff(assets, collect(keys(universe.data)))) == 0
+@testset "Universe" begin
+    @testset "Construct" begin
+        universe = Universe(assets)
+    end
+    @testset "Gather" begin
+        function datasource(asset::String; save_downloads::Bool=true)::TS
+            savedata_path = Pkg.dir("Strategems", "data/$asset.csv")
+            if isfile(savedata_path)
+                return Temporal.tsread(savedata_path)
+            else
+                X = quandl(asset)
+                if save_downloads
+                    Temporal.tswrite(X, savedata_path)
+                end
+                return X
+            end
+        end
+        gather!(universe, source=datasource)
+    end
+end
 
 # define indicators and parameter space
-arg_names = [:fastlimit, :slowlimit]
-arg_defaults = [0.5, 0.05]
-#FIXME: get_param_combos breaking with other argument ranges
-arg_ranges = [0.05:0.25:0.99, 0.05:0.25:0.95]
-paramset = ParameterSet(arg_names, arg_defaults, arg_ranges)
-@test paramset.arg_names == arg_names
-f(x; args...) = Indicators.mama(x; args...)
-indicator = Indicator(f, paramset)
+@testset "Parameter Set" begin
+    arg_names     = [:fastlimit, :slowlimit]
+    arg_defaults  = [0.5, 0.05]
+    arg_ranges    = [0.01:0.01:0.99, 0.01:0.01:0.99]
+    paramset      = ParameterSet(arg_names, arg_defaults, arg_ranges)
+end
 
-# define signals
-#signals = Dict{Symbol,Signal}(:GoLong=>Signal(:(MAMA ↑ FAMA)), :GoShort=>Signal(:(MAMA ↓ FAMA)))
-siglong = @signal MAMA ↑ FAMA
-sigshort = @signal MAMA ↓ FAMA
-signals = Dict{Symbol,Signal}(:GoLong => siglong, :GoShort => sigshort)
+@testset "Indicator" begin
+    f(x; args...) = Indicators.mama(x; args...)
+    indicator     = Indicator(f, paramset)
+end
 
-# define the trading rule
-rules = Dict{Symbol,Rule}(:EnterLong=>Rule(:GoLong, :(buy,asset,100)),
-                          :EnterShort=>Rule(:GoShort, :(sell,asset,100)))
+# define signals that will trigger trading decisions
+@testset "Signal" begin
+    @testset "Construct" begin
+        siglong  = @signal MAMA ↑ FAMA
+        sigshort = @signal MAMA ↓ FAMA
+        sigexit  = @signal MAMA .== FAMA
+    end
+end
 
-# strategy object
-strat = Strategy(universe, indicator, signals, rules)
-generate_trades!(strat)
-backtest!(strat)
-optimize!(strat, samples=4)
-@test size(strat.results.optimization,1) == 4
-@test size(strat.results.optimization,2) == length(arg_names)+1
+# define the trading rules
+@testset "Rule" begin
+    @testset "Construct" begin
+        longrule  = @rule siglong → long 100
+        shortrule = @rule sigshort → short 100
+        exitrule  = @rule sigexit → liquidate 1.0
+        rules     = (longrule, shortrule, exitrule)
+    end
+end
+
+# run strategy
+@testset "Strategy" begin
+    @testset "Construct" begin
+        strat = Strategy(universe, indicator, rules)
+    end
+    @testset "Backtest" begin
+        backtest!(strat)
+    end
+    @testset "Optimize" begin
+        optimize!(strat, samples=10)
+    end
+end
