823e078040b3ba421fca898946e93f2373c3e46c
dysonance <jamos125@gmail.com>
2017-11-15 07:49:06 -0500

    updated/fixed test cases including new macro api syntax

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,66 +5,72 @@ using Base.Test
 assets = ["Corn"]
 @testset "Universe" begin
     @testset "Construct" begin
-        universe = Universe(assets)
+        global universe = Universe(assets)
+        @test universe.assets == assets
     end
     @testset "Gather" begin
-        function datasource(asset::String; save_downloads::Bool=true)::TS
-            savedata_path = Pkg.dir("Strategems", "data/$asset.csv")
-            if isfile(savedata_path)
-                return Temporal.tsread(savedata_path)
-            else
-                X = quandl(asset)
-                if save_downloads
-                    Temporal.tswrite(X, savedata_path)
-                end
-                return X
-            end
-        end
-        gather!(universe, source=datasource)
+        # function datasource(asset::String; save_downloads::Bool=true)::TS
+        #     savedata_path = Pkg.dir("Strategems", "data/$asset.csv")
+        #     if isfile(savedata_path)
+        #         return Temporal.tsread(savedata_path)
+        #     else
+        #         X = quandl(asset)
+        #         if save_downloads
+        #             Temporal.tswrite(X, savedata_path)
+        #         end
+        #         return X
+        #     end
+        # end
+        # gather!(universe, source=datasource)
+        gather!(universe, source=(asset)->Temporal.tsread("$(Pkg.dir("Temporal"))/data/$asset.csv"))
+        @test length(setdiff(assets, collect(keys(universe.data)))) == 0
     end
 end
 
 # define indicators and parameter space
 @testset "Parameter Set" begin
-    arg_names     = [:fastlimit, :slowlimit]
-    arg_defaults  = [0.5, 0.05]
-    arg_ranges    = [0.01:0.01:0.99, 0.01:0.01:0.99]
-    paramset      = ParameterSet(arg_names, arg_defaults, arg_ranges)
+    global arg_names          = [:fastlimit, :slowlimit]
+    global arg_defaults       = [0.5, 0.05]
+    global arg_ranges         = [0.01:0.01:0.99, 0.01:0.01:0.99]
+    global paramset           = ParameterSet(arg_names, arg_defaults, arg_ranges)
+    @test paramset.arg_names == arg_names
 end
 
 @testset "Indicator" begin
-    f(x; args...) = Indicators.mama(x; args...)
-    indicator     = Indicator(f, paramset)
+    global f(x; args...) = Indicators.mama(x; args...)
+    global indicator     = Indicator(f, paramset)
 end
 
 # define signals that will trigger trading decisions
 @testset "Signal" begin
     @testset "Construct" begin
-        siglong  = @signal MAMA ↑ FAMA
-        sigshort = @signal MAMA ↓ FAMA
-        sigexit  = @signal MAMA .== FAMA
+        global siglong  = @signal MAMA ↑ FAMA
+        global sigshort = @signal MAMA ↓ FAMA
+        global sigexit  = @signal MAMA .== FAMA
     end
 end
 
 # define the trading rules
 @testset "Rule" begin
     @testset "Construct" begin
-        longrule  = @rule siglong → long 100
-        shortrule = @rule sigshort → short 100
-        exitrule  = @rule sigexit → liquidate 1.0
-        rules     = (longrule, shortrule, exitrule)
+        global longrule  = @rule siglong → long 100
+        global shortrule = @rule sigshort → short 100
+        global exitrule  = @rule sigexit → liquidate 1.0
+        global rules     = (longrule, shortrule, exitrule)
     end
 end
 
 # run strategy
 @testset "Strategy" begin
     @testset "Construct" begin
-        strat = Strategy(universe, indicator, rules)
+        global strat = Strategy(universe, indicator, rules)
     end
     @testset "Backtest" begin
         backtest!(strat)
     end
     @testset "Optimize" begin
         optimize!(strat, samples=10)
+        @test size(strat.results.optimization,1) == 10
+        @test size(strat.results.optimization,2) == length(arg_names)+1
     end
 end
