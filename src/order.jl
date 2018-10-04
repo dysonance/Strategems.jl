c5298bc1fbcfb84ec5b8a8aee997800fc7ab6aad
Jacob Amos <jamos125@gmail.com>
2018-10-04 16:40:40 -0400

    re-add order file that was killed in the merge

---
+++ src/order.jl
@@ -0,0 +1,37 @@
+#=
+types and methods to facilitate the interaction between orders, rules, and portfolios
+=#
+
+abstract type AbstractOrder end
+
+struct MarketOrder <: AbstractOrder
+    asset::String
+    quantity::Number
+end
+
+struct LimitOrder <: AbstractOrder
+    asset::String
+    quantity::Number
+    limit::Number
+end
+
+struct StopOrder <: AbstractOrder
+    asset::String
+    quantity::Number
+    stop::Number
+end
+
+#TODO: complete this logic, enable interaction with strategy/portfolio objects
+#TODO: diffierentiate between buying vs. going long and the like (the latter should reverse position if short)
+
+#TODO: add logic whereby the order logic is altered by the type `T` of qty
+# (if T<:Int, order that many *shares*, else if T<:Float64, interpret qty as a fraction of the portfolio at time t)
+
+liquidate{T<:Real}(qty::T) = qty
+
+long{T<:Real}(qty::T)::T = qty
+buy{T<:Real}(qty::T)::T = qty
+
+short{T<:Real}(qty::T)::T = qty
+sell{T<:Real}(qty::T)::T = qty
+
