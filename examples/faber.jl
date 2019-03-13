4f8e185582f30881b49823829c8799921df41f68
Jacob Amos <jamos125@gmail.com>
2019-03-12 21:30:11 -0400

    update faber example

--- examples/faber.jl
+++ examples/faber.jl
@@ -1,11 +1,28 @@
-using Strategems, Temporal, Indicators
+using Strategems, Temporal, Indicators, Plots
 using Dates
 using Test
 
 # define universe and gather data
 assets = ["EOD/AAPL", "EOD/MCD", "EOD/JPM", "EOD/MMM", "EOD/XOM"]
 universe = Universe(assets)
-gather!(universe)
+
+function datasource(asset::String; save_downloads::Bool=true)::TS
+    path = joinpath(dirname(pathof(Strategems)), "..", "data", "test", "$asset.csv")
+    if isfile(path)
+        return Temporal.tsread(path)
+    else
+        X = quandl(asset)
+        if save_downloads
+            if !isdir(dirname(path))
+                mkdir(dirname(path))
+            end
+            Temporal.tswrite(X, path)
+        end
+        return X
+    end
+end
+
+gather!(universe, source=datasource)
 
 # define indicator and parameters
 function fun(x::TS; args...)::TS
@@ -16,21 +33,25 @@ function fun(x::TS; args...)::TS
     return output
 end
 
-#TODO: define method for when only one parameter is needed where it automatically puts things in vectors
+# TODO: define method for when only one parameter is needed where it automatically puts things in vectors
 # (so that you could call this by doing `ParameterSet(:n, 50)`)
 indicator = Indicator(fun, ParameterSet([:n], [50]))
 
 # define signals
-signals = Dict{Symbol,Signal}(:GoLong => Signal(:(Close ↑ MA)),
-                              :GoShort => Signal(:(Close ↓ MA)))
+longsignal = @signal Close ↑ MA
+shortsignal = @signal Close ↓ MA
 
 # define trading rules
-rules = Dict{Symbol,Rule}(:EnterLong => Rule(:GoLong, :(buy,asset,100)),
-                          :EnterShort => Rule(:GoShort, :(sell,asset,100)))
+longrule = @rule longsignal → buy 100
+shortrule = @rule shortsignal → sell 100
 
 # construct and test the strategy
-strat = Strategy(universe, indicator, signals, rules)
+strat = Strategy(universe, indicator, (longrule, shortrule))
 
-#TODO: print diagnostic information while running backtest
 backtest!(strat, px_trade=:Open, px_close=:Close)
 
+weights, holdings, values, profits = summarize_results(strat)
+# p1 = plot(cumsum(profits))
+# p2 = plot(profits)
+# p3 = plot(weights)
+# plot(p1, p2, p3, layout=(3,1))
