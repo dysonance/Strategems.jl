de703a335859b8790235c5e35ab3b63877c4f48d
Jacob Amos <jamos125@gmail.com>
2019-03-20 02:55:29 -0400

    add union index as member of universe

--- src/model/universe.jl
+++ src/model/universe.jl
@@ -6,28 +6,20 @@ using ProgressMeter
 
 import Base: show
 
-const SEPARATORS = ['/', '_', '.']
-
-# function guess_tickers(assets::Vector{String})::Vector{Symbol}
-#     tickers = Symbol.([Temporal.namefix(split(asset, SEPARATORS)[end]) for asset in assets])
-#     @assert tickers == unique(tickers)  "Non-unique ticker symbols found in universe"
-#     return tickers
-# end
-
 mutable struct Universe
     assets::Vector{String}
-    # tickers::Vector{Symbol}
     data::Dict{String,TS}
     from::TimeType
     thru::TimeType
+    index::Vector{TimeType}
     function Universe(assets::Vector{String}, from::TimeType=Dates.Date(0), thru::TimeType=Dates.today())
         @assert assets == unique(assets)
-        # tickers = guess_tickers(assets)
         data = Dict{String,TS}()
         @inbounds for asset in assets
             data[asset] = TS()
         end
-        return new(assets, data, from, thru)
+        index = union((data[asset].index for asset in assets)...)
+        return new(assets, data, from, thru, index)
     end
 end
 
@@ -46,15 +38,6 @@ function gather!(universe::Universe; source::Function=Temporal.quandl, verbose::
     universe.from = max(minimum(t0), universe.from)
     universe.thru = min(maximum(tN), universe.thru)
     return nothing
-end
-
-#FIXME: make robust to other time types
-function get_overall_index(universe::Universe)::Vector{Date}
-    idx = Vector{Date}()
-    for asset in universe.assets
-        idx = union(idx, universe.data[asset].index)
-    end
-    return idx
 end
 
 function show(io::IO, universe::Universe)
