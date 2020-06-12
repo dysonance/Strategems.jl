a74b3ba76736372c40ccd77c0dbcc058e0b4194a
Hantao Cui <cuihantao@gmail.com>
2020-06-11 20:55:34 -0400

    made Indicator immutable

--- src/model/indicator.jl
+++ src/model/indicator.jl
@@ -1,14 +1,17 @@
 
 import Base: show
 
-mutable struct Indicator
+struct Indicator
     fun::Function
     paramset::ParameterSet
     data::TS
-    function Indicator(fun::Function, paramset::ParameterSet)
-        data = TS()
+    function Indicator(fun::Function, paramset::ParameterSet, data::TS)
         return new(fun, paramset, data)
     end
+end
+
+function Indicator(fun::Function, paramset::ParameterSet)
+    return Indicator(fun, paramset, TS())
 end
 
 function calculate(indicator::Indicator, input::TS)::TS
@@ -20,15 +23,15 @@ end
 #     return nothing
 # end
 
-function generate_dict(universe::Universe, indicator::Indicator)::Dict{String,Indicator}
-    indicators = Dict{String,Indicator}()
-    for asset in universe.assets
-        local ind = Indicator(indicator.fun, indicator.paramset)
-        calculate!(ind, universe.data[asset])
-        indicators[asset] = ind
-    end
-    return indicators
-end
+# function generate_dict(universe::Universe, indicator::Indicator)::Dict{String,Indicator}
+#     indicators = Dict{String,Indicator}()
+#     for asset in universe.assets
+#         local ind = Indicator(indicator.fun, indicator.paramset)
+#         calculate!(ind, universe.data[asset])
+#         indicators[asset] = ind
+#     end
+#     return indicators
+# end
 
 # TODO: add information about the calculation function
 function show(io::IO, indicator::Indicator)
